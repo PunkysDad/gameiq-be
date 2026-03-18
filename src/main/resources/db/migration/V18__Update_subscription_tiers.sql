9a297dc2fcaff0248562e3c22b0a7acdc3440a06
PunkysDad <punkysdad@yahoo.com>
2026-03-17 20:03:45 -0600

    Removing migrations

--- src/main/resources/db/migration/V18__Update_subscription_tiers.sql
+++
@@ -1,8 +0,0 @@
--- Update subscription_tier default from FREE to TRIAL
--- Column is varchar(50) so no enum type changes needed
-
-ALTER TABLE users
-    ALTER COLUMN subscription_tier SET DEFAULT 'TRIAL';
-
--- Update any existing FREE users to TRIAL (safety net)
-UPDATE users SET subscription_tier = 'TRIAL' WHERE subscription_tier = 'FREE';
