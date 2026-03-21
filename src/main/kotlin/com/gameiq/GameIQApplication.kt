e45b78fbc1fe80c61cacf33c61d1f312a66ee1fa
PunkysDad <punkysdad@yahoo.com>
2026-03-21 14:19:11 -0600

    Troubleshooting aws

--- src/main/kotlin/com/gameiq/GameIQApplication.kt
+++ src/main/kotlin/com/gameiq/GameIQApplication.kt
@@ -1,12 +1,13 @@
 package com.gameiq
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.transaction.annotation.EnableTransactionManagement
 import org.springframework.scheduling.annotation.EnableScheduling
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [UserDetailsServiceAutoConfiguration::class])
 @EnableJpaRepositories
 @EnableTransactionManagement
 @EnableScheduling
